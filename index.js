752ce164866907946afb24635b6ea1967ae45361
RoseVallea <91046826+RoseVallea@users.noreply.github.com>
2024-08-04 14:13:03 +0800

    index.js

--- index.js
+++ index.js
@@ -72,7 +72,7 @@ function updateStatusAndSendMessages() {
   const nextStatus = statusMessages[(currentIndex + 1) % statusMessages.length];
 
   client.user.setPresence({
-    activities: [{ name: currentStatus, type: ActivityType.Watching}],
+    game: [{ name: currentStatus, type: "WATCHING"}],
     status: 'dnd',
   });
 
