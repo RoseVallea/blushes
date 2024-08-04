498c258f2f442f617c1058f01d20ade4b82f6b75
RoseVallea <91046826+RoseVallea@users.noreply.github.com>
2024-08-04 13:25:28 +0800

    index.js

--- index.js
+++ index.js
@@ -36,8 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["over bubblegum ﹒ ♡","over bubblegum ﹒ ♡ ","over bubblegum ﹒ ♡"," over bubblegum ﹒ ♡","over bubblegum ﹒ ♡ ","over bubblegum ﹒ ♡","over bubblegum ﹒ ♡ ","over bubblegum ﹒ ♡ ","over bubblegum﹒ ♡"];
-
+const statusMessages = ["over bubblegum ﹒ ♡"]
 
 let currentIndex = 0;
 const channelId = '';
@@ -73,7 +72,7 @@ function updateStatusAndSendMessages() {
   const nextStatus = statusMessages[(currentIndex + 1) % statusMessages.length];
 
   client.user.setPresence({
-    activities: [{ name: currentStatus, type: ActivityType.Watching}],
+    activities: [{ name: currentStatus, type: ActivityType.Custom}],
     status: 'dnd',
   });
 
