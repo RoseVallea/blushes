513db82a5280729db3f4980fa4df0e8e6d9dcefe
RoseVallea <91046826+RoseVallea@users.noreply.github.com>
2024-08-04 12:25:08 +0800

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["over bubblegum ﹒ ♡"];
+const statusMessages = ["over bubblegum ﹒ ♡","over bubblegum ﹒ ♡","over bubblegum ﹒ ♡","over bubblegum ﹒ ♡","over bubblegum ﹒ ♡","over bubblegum ﹒ ♡"];
 
 
 let currentIndex = 0;
@@ -74,7 +74,7 @@ function updateStatusAndSendMessages() {
 
   client.user.setPresence({
     activities: [{ name: currentStatus, type: ActivityType.Watching}],
-    status: 'online',
+    status: 'dnd',
   });
 
   
